bdbc1a237c33386cffb53bdcf75b17581beb1c6d
Sandor Semsey <sandor@es-progress.hu>
2021-04-06 17:27:38 +0200

    inspections

--- CRM/RcBase/Api/Get.php
+++ CRM/RcBase/Api/Get.php
@@ -45,7 +45,7 @@ class CRM_RcBase_Api_Get
      *
      * @return mixed|null
      */
-    protected static function parseResultsFirst(Result $results, string $field = "")
+    protected static function parseResultsFirst(Result $results, string $field = '')
     {
         // Get first result row
         $result = $results->first();
