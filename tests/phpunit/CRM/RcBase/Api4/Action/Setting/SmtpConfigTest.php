53f1a636421f34639e3f699b3286eefd7cb24e2f
Sandor Semsey <sandor@es-progress.hu>
2022-01-13 21:31:07 +0100

    Test setconfig

--- tests/phpunit/CRM/RcBase/Api4/Action/Setting/SmtpConfigTest.php
+++ tests/phpunit/CRM/RcBase/Api4/Action/Setting/SmtpConfigTest.php
@@ -13,10 +13,9 @@ class CRM_RcBase_Api4_SmtpConfigTest extends CRM_RcBase_HeadlessTestCase
         = [
             'smtpServer' => 'smtp.example.com',
             'smtpPort' => '465',
-            'smtpAuth' => '1',
+            'smtpAuth' => false,
             'smtpUsername' => 'admin',
             'smtpPassword' => 'pass',
-            'smtpPasswordfdsf' => 'pass',
         ];
 
     /**
@@ -70,9 +69,75 @@ class CRM_RcBase_Api4_SmtpConfigTest extends CRM_RcBase_HeadlessTestCase
             ->setConfig('invalid_config')
             ->execute();
         self::assertCount(1, $smtp_configs, 'Failed to get configs');
-        self::assertArrayHasKey('is_error', $smtp_configs[0], "Missing is_error");
-        self::assertArrayHasKey('error_message', $smtp_configs[0], "Missing is_error");
+        self::assertArrayHasKey('is_error', $smtp_configs[0], 'Missing is_error');
+        self::assertArrayHasKey('error_message', $smtp_configs[0], 'Missing error_message');
         self::assertEquals(true, $smtp_configs[0]['is_error']);
         self::assertEquals('Not allowed config: invalid_config', $smtp_configs[0]['error_message']);
+    }
+
+    /**
+     * @return void
+     * @throws \API_Exception
+     * @throws \Civi\API\Exception\UnauthorizedException
+     */
+    public function testSetConfig()
+    {
+        $new_configs = [
+            'server' => 'other.smtp.example.com',
+            'port' => 112,
+            'need_auth' => true,
+            'user' => 'other_admin',
+            'pass' => 'other_pass',
+        ];
+
+        Setting::setSmtpConfig()
+            ->setServer($new_configs['server'])
+            ->setPort($new_configs['port'])
+            ->setUser($new_configs['user'])
+            ->setPass($new_configs['pass'])
+            ->setNeedAuth($new_configs['need_auth'])
+            ->execute();
+
+        $smtp_configs = Setting::get()
+            ->addSelect('mailing_backend')
+            ->execute();
+        self::assertCount(1, $smtp_configs, 'Failed to get configs');
+        self::assertArrayHasKey('value', $smtp_configs[0], 'Failed to get configs');
+        self::assertArrayHasKey('smtpServer', $smtp_configs[0]['value'], 'Missing config: smtpServer');
+        self::assertEquals($new_configs['server'], $smtp_configs[0]['value']['smtpServer']);
+        self::assertArrayHasKey('smtpPort', $smtp_configs[0]['value'], 'Missing config: smtpPort');
+        self::assertEquals($new_configs['port'], $smtp_configs[0]['value']['smtpPort']);
+        self::assertArrayHasKey('smtpAuth', $smtp_configs[0]['value'], 'Missing config: smtpAuth');
+        self::assertEquals($new_configs['need_auth'], $smtp_configs[0]['value']['smtpAuth']);
+        self::assertArrayHasKey('smtpUsername', $smtp_configs[0]['value'], 'Missing config: smtpUsername');
+        self::assertEquals($new_configs['user'], $smtp_configs[0]['value']['smtpUsername']);
+    }
+
+    /**
+     * @return void
+     */
+    public function testSetNoParamsReturnEmpty()
+    {
+        $results = Setting::setSmtpConfig()
+            ->execute();
+        self::assertCount(0, $results, 'Not empty result set when no params to set');
+    }
+
+    /**
+     * @return void
+     */
+    public function testPasswordChange()
+    {
+        $new_pass = 'new_password';
+        Setting::setSmtpConfig()
+            ->setPass($new_pass)
+            ->execute();
+
+        $pass = Setting::getSmtpConfig()
+            ->setConfig('pass')
+            ->execute();
+        self::assertCount(1, $pass, 'Failed to get password');
+        self::assertArrayHasKey('pass', $pass[0], 'Failed to get password');
+        self::assertEquals($new_pass, $pass[0]['pass'], 'Failed to change password');
     }
 }
