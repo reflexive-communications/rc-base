8defcb0d6ec59eb6e8fd1dd4fd50a62ca697c6ca
Sandor Semsey <sandor@es-progress.hu>
2022-09-01 01:01:16 +0200

    Utils\Test::createIndividual

--- Civi/RcBase/Utils/Test.php
+++ Civi/RcBase/Utils/Test.php
@@ -14,7 +14,6 @@ class Test
      * Simulate a logged in user
      *
      * @return int Contact ID
-     *
      * @throws \CRM_Core_Exception
      */
     public static function createLoggedInUser(): int
@@ -42,6 +41,52 @@ class Test
         // Set ID in session
         $session = CRM_Core_Session::singleton();
         $session->set('userID', $contact_id);
+
+        return $contact_id;
+    }
+
+    /**
+     * Create contact
+     *
+     * @param int $counter Contact serial number
+     * @param array $extra Extra parameters to Contact entity
+     *
+     * @return int Contact ID
+     * @throws \CRM_Core_Exception
+     */
+    public static function createIndividual(int $counter, array $extra = []): int
+    {
+        $default = [
+            'contact_type' => 'Individual',
+            'first_name' => "user_{$counter}",
+            'middle_name' => 'middle',
+            'last_name' => 'Test',
+            'external_identifier' => $counter,
+        ];
+
+        return CRM_RcBase_Api_Create::contact(array_merge($default, $extra));
+    }
+
+    /**
+     * Create contact with email address
+     *
+     * @param int $counter Contact serial number
+     * @param array $extra_contact Extra parameters to Contact entity
+     * @param array $extra_email Extra parameters to Email entity
+     *
+     * @return int Contact ID
+     * @throws \CRM_Core_Exception
+     */
+    public static function createIndividualWithEmail(int $counter, array $extra_contact = [], array $extra_email = []): int
+    {
+        $default = [
+            'location_type_id' => 1,
+            'email' => "user_{$counter}@test.com",
+            'is_primary' => true,
+        ];
+
+        $contact_id = self::createIndividual($counter, $extra_contact);
+        CRM_RcBase_Api_Create::email($contact_id, array_merge($default, $extra_email));
 
         return $contact_id;
     }
--- tests/phpunit/Civi/RcBase/Utils/TestTest.php
+++ tests/phpunit/Civi/RcBase/Utils/TestTest.php
@@ -2,7 +2,9 @@
 
 namespace Civi\RcBase\Utils;
 
+use Civi\Api4\Email;
 use CRM_Core_Session;
+use CRM_RcBase_Api_Get;
 use CRM_RcBase_HeadlessTestCase;
 
 /**
@@ -22,5 +24,51 @@ class TestTest extends CRM_RcBase_HeadlessTestCase
         $session = CRM_Core_Session::singleton();
         self::assertSame($contact_id, CRM_Core_Session::getLoggedInContactID(), 'Contact ID not set in session');
         self::assertSame('logged_in user', $session->getLoggedInContactDisplayName(), 'Wrong name for logged in user');
+    }
+
+    /**
+     * @return void
+     * @throws \API_Exception
+     * @throws \CRM_Core_Exception
+     * @throws \Civi\API\Exception\UnauthorizedException
+     */
+    public function testCreateIndividual()
+    {
+        $counter = 5;
+        $values = [
+            'api_key' => 'some_api_key',
+            'last_name' => 'Other-test',
+        ];
+        $contact_id = Test::createIndividual($counter, $values);
+
+        $contact = CRM_RcBase_Api_Get::contactData($contact_id);
+        self::assertSame($values['api_key'], $contact['api_key'], 'Wrong api_key returned');
+        self::assertSame($values['last_name'], $contact['last_name'], 'Wrong last_name returned');
+        self::assertSame("user_{$counter}", $contact['first_name'], 'Wrong first_name returned');
+    }
+
+    /**
+     * @return void
+     * @throws \API_Exception
+     * @throws \CRM_Core_Exception
+     * @throws \Civi\API\Exception\UnauthorizedException
+     */
+    public function createIndividualWithEmail()
+    {
+        $counter = 42;
+        $values = [
+            'signature_text' => 'Regards, testing',
+            'invalid_param' => 'invalid value',
+        ];
+        $contact_id = Test::createIndividualWithEmail($counter, $values);
+
+        self::assertSame($contact_id, CRM_RcBase_Api_Get::contactIDFromEmail("user_{$counter}@test.com"), 'Failed to create contact with email');
+
+        $emails = Email::get()
+            ->addWhere('email', '=', "user_{$counter}@test.com")
+            ->setLimit(1)
+            ->execute()
+            ->first();
+        self::assertSame($values['signature_text'], $emails['signature_text'], 'Wrong signature_text returned');
     }
 }
