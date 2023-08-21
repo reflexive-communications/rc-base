cf298918343492952c65c11107be48fb9de6d682
Sandor Semsey <sandor@es-progress.hu>
2023-08-21 02:06:39 +0200

    Strings::removeComments

--- tests/phpunit/Civi/RcBase/Utils/StringsTest.php
+++ tests/phpunit/Civi/RcBase/Utils/StringsTest.php
@@ -23,4 +23,20 @@ class StringsTest extends HeadlessTestCase
         $expected = ' not trimmed ';
         self::assertSame($expected, Strings::compactWhitespace($input, false), 'Wrong compacted string');
     }
+
+    /**
+     * @return void
+     */
+    public function testRemoveComments()
+    {
+        $input = "this is a test /* with comment */ string // with comment\nand a new line\n";
+        $expected = "this is a test  string \nand a new line\n";
+        self::assertSame($expected, Strings::removeComments($input), 'Wrong filtered string');
+
+        $input = '/* comment with @@ */ and unbalanced comment */';
+        $expected = ' and unbalanced comment */';
+        self::assertSame($expected, Strings::removeComments($input), 'Wrong filtered string');
+
+        self::assertSame('', Strings::removeComments(''), 'Wrong filtered string');
+    }
 }
