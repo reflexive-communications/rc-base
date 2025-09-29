4078e7fbe84e39d6ab29a3449af03f4cf831ee56
Sandor Semsey <sandor@es-progress.hu>
2025-09-29 12:26:05 +0200

    delete-orphans: delete orphans where orig_field is null

--- sql/delete-orphans.sql
+++ sql/delete-orphans.sql
@@ -13,20 +13,32 @@ CREATE OR REPLACE PROCEDURE civicrm_delete_orphans(
 )
 MODIFIES SQL DATA COMMENT "Delete orphan records (that refer to a non-existing record) from a table"
 BEGIN
+    DECLARE deleted_nulls INT DEFAULT 0;
+    DECLARE deleted_orphans INT DEFAULT 0;
+
+    SET @sql = CONCAT('DELETE FROM `', orig_table, '` WHERE `', orig_field, '` IS NULL');
+    PREPARE stmt FROM @sql;
+    EXECUTE stmt;
+    SET deleted_nulls = ROW_COUNT();
+    DEALLOCATE PREPARE stmt;
+
     CREATE TEMPORARY TABLE tmp_orphans (id BIGINT PRIMARY KEY);
     SET @sql = CONCAT(
-        "INSERT INTO tmp_orphans (id) SELECT orig.`", orig_field, "` FROM `", orig_table, "` orig ",
-        "LEFT JOIN `", ref_table, "` ref ON orig.`", orig_field, "` = ref.`", ref_field, "` ",
-        "WHERE ref.`", ref_field, "` IS NULL");
+        'INSERT INTO tmp_orphans (id) ',
+        'SELECT orig.`', orig_field, '` FROM `', orig_table, '` orig ',
+        'LEFT JOIN `', ref_table, '` ref ON orig.`', orig_field, '` = ref.`', ref_field, '` ',
+        'WHERE ref.`', ref_field, '` IS NULL');
     PREPARE stmt FROM @sql;
     EXECUTE stmt;
     DEALLOCATE PREPARE stmt;
 
-    SET @sql = CONCAT("DELETE FROM `", orig_table, "` WHERE `", orig_field, "` IN (SELECT id FROM tmp_orphans)");
+    SET @sql = CONCAT('DELETE FROM `', orig_table, '` WHERE `', orig_field, '` IN (SELECT id FROM tmp_orphans)');
     PREPARE stmt FROM @sql;
     EXECUTE stmt;
-    SET affected_rows = ROW_COUNT();
+    SET deleted_orphans = ROW_COUNT();
     DEALLOCATE PREPARE stmt;
+
+    SET affected_rows = deleted_nulls + deleted_orphans;
 
     DROP TEMPORARY TABLE IF EXISTS tmp_orphans;
 END
--- tests/phpunit/Civi/RcBase/Utils/DBTest.php
+++ tests/phpunit/Civi/RcBase/Utils/DBTest.php
@@ -230,6 +230,7 @@ class DBTest extends HeadlessTestCase
             $contact_id = PHPUnit::createIndividualWithEmail();
             Remove::entity('Contact', $contact_id);
         }
+        DB::query('INSERT INTO civicrm_email (email, contact_id) VALUES ("contact_id.is.null@example.com", NULL)');
 
         // Try to add FK back - should fail
         try {
@@ -241,9 +242,14 @@ class DBTest extends HeadlessTestCase
         DB::query('CALL civicrm_delete_orphans("civicrm_email", "contact_id", "civicrm_contact", "id", @affected)');
         $result = DB::query('SELECT @affected');
         self::assertCount(1, $result, 'Wrong number of result rows');
-        self::assertEquals(5, $result[0]['@affected'], 'Wrong number of affected rows');
+        self::assertEquals(6, $result[0]['@affected'], 'Wrong number of affected rows');
 
         // Add back FK - should work now
         DB::query('ALTER TABLE civicrm_email ADD CONSTRAINT FK_civicrm_email_contact_id FOREIGN KEY (contact_id) REFERENCES civicrm_contact(id) ON DELETE CASCADE');
+
+        DB::query('CALL civicrm_delete_orphans("civicrm_email", "contact_id", "civicrm_contact", "id", @affected)');
+        $result = DB::query('SELECT @affected');
+        self::assertCount(1, $result, 'Wrong number of result rows');
+        self::assertEquals(0, $result[0]['@affected'], 'Wrong number of affected rows');
     }
 }
