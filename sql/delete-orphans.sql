bc04f09c72e9997b96493e0287245112b22d2cff
Sandor Semsey <sandor@es-progress.hu>
2025-09-30 13:53:08 +0200

    delete-orphans: fix to not delete rows with NULL as it may be allowed

--- sql/delete-orphans.sql
+++ sql/delete-orphans.sql
@@ -13,22 +13,13 @@ CREATE OR REPLACE PROCEDURE civicrm_delete_orphans(
 )
 MODIFIES SQL DATA COMMENT "Delete orphan records (that refer to a non-existing record) from a table"
 BEGIN
-    DECLARE deleted_nulls INT DEFAULT 0;
-    DECLARE deleted_orphans INT DEFAULT 0;
-
-    SET @sql = CONCAT('DELETE FROM `', orig_table, '` WHERE `', orig_field, '` IS NULL');
-    PREPARE stmt FROM @sql;
-    EXECUTE stmt;
-    SET deleted_nulls = ROW_COUNT();
-    DEALLOCATE PREPARE stmt;
-
     CREATE TEMPORARY TABLE tmp_orphans (id BIGINT PRIMARY KEY);
     SET @sql = CONCAT(
         'INSERT INTO tmp_orphans (id) ',
-        'SELECT DISTINCT orig.`', orig_field,
-        '` FROM `', orig_table, '` orig ',
+        'SELECT DISTINCT orig.`', orig_field, '` ',
+        'FROM `', orig_table, '` orig ',
         'LEFT JOIN `', ref_table, '` ref ON orig.`', orig_field, '` = ref.`', ref_field, '` ',
-        'WHERE ref.`', ref_field, '` IS NULL');
+        'WHERE ref.`', ref_field, '` IS NULL AND orig.`', orig_field, '` IS NOT NULL');
     PREPARE stmt FROM @sql;
     EXECUTE stmt;
     DEALLOCATE PREPARE stmt;
@@ -36,10 +27,8 @@ BEGIN
     SET @sql = CONCAT('DELETE FROM `', orig_table, '` WHERE `', orig_field, '` IN (SELECT id FROM tmp_orphans)');
     PREPARE stmt FROM @sql;
     EXECUTE stmt;
-    SET deleted_orphans = ROW_COUNT();
+    SET affected_rows = ROW_COUNT();
     DEALLOCATE PREPARE stmt;
-
-    SET affected_rows = deleted_nulls + deleted_orphans;
 
     DROP TEMPORARY TABLE IF EXISTS tmp_orphans;
 END
